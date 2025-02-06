857f5b0c8681cd4a17f5e83d0115ebe8dc9ef761
jianxun-qu <75991617+jianxun-qu@users.noreply.github.com>
2025-02-06 15:34:06 +0800

    Add files via upload

---
+++ process_reg.m
@@ -0,0 +1,11 @@
+t1_bravo_nii = 't1_bravo.nii';
+gre_ref_nii = 'gre_ref.nii';
+na_hr_nii = 'c2p_hr.nii';
+
+auxil_spm_reslice({gre_ref_nii}, {na_hr_nii}, 'prefix', 'rslc_');
+
+na_hr_nii = ['rslc_', na_hr_nii];
+
+auxil_spm_coreg({t1_bravo_nii}, {gre_ref_nii, na_hr_nii}, 'prefix', 'reg_');
+
+na_hr_nii = ['reg_', na_hr_nii];
